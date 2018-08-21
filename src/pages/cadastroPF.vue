56a2fa4247e6bc7216cf8559243956c39212b06a
hugomsm <hugomaiadasilvamarques@gmail.com>
2018-08-21 12:03:25 -0300

    mask-cpf

--- src/pages/cadastroPF.vue
+++ src/pages/cadastroPF.vue
@@ -1,22 +1,22 @@
 <template>
   <q-page padding>
-    <div class='q-title' align='center'>CADASTRO DE PESSOA FÍSICA</div>
-    <q-btn rounded color=primary icon='arrow_back' to='/clientes'>Voltar</q-btn>
+    <div class='q-title' align='center'><q-btn round     color=primary icon='arrow_back' to='/clientes'></q-btn>CADASTRO DE PESSOA FÍSICA</div>
+    
     <div>
       <q-field icon="face">
         <q-input clearable="true" v-model="nomeClienteF" float-label="Nome do cliente" />
       </q-field>
       <q-field icon="fingerprint">
-        <q-input type='number' v-model="cpfClienteF" float-label="CPF do cliente" />
+        <q-input type='text' v-model="cpfClienteF" v-mask="'999.999.999-99'" float-label="CPF do cliente" />
       </q-field>
       <q-field icon="place">
         <q-input type='textarea' clearable="true" v-model="enderecoClienteF" float-label="Endereço do cliente" />
       </q-field>
       <q-field icon="phone">
-        <q-input type='tel' v-model="telefoneClienteF" float-label="Telefone do cliente" />
+        <q-input type='tel' v-model="telefoneClienteF" v-mask="'(99) 99999-9999'" float-label="Telefone do cliente" />
       </q-field>
       <q-field icon="alternate_email">
-        <q-input type='email' clearable="true" v-model="emailClienteF" float-label="E-mail do cliente" />
+        <q-input type='email' clearable="true" v-mask="'999.999.999-99'" v-model="emailClienteF" float-label="E-mail do cliente" />
       </q-field>
       <q-field icon="description">
         <q-input type='textarea' clearable="true" v-model="obsClienteF" float-label="Observações" />
@@ -32,6 +32,7 @@
 </template>
 
 <script>
+import AwesomeMask from "awesome-mask";
 export default {
   data() {
     return {
@@ -52,6 +53,9 @@ export default {
         (this.emailClienteF = ""),
         (this.obsClienteF = "");
     }
+  },
+  directives: {
+    mask: AwesomeMask
   }
 };
 </script>
